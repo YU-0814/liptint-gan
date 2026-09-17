33b4eea78b18c7b3f29bc3416a0dfabca12af77e
yu jin yeong <c1audem0x@gmail.com>
2026-09-18 05:26:59 +0900

    Tidy docstring

--- pix2pix.py
+++ pix2pix.py
@@ -1,5 +1,4 @@
-"""U-Net generator, 70x70 PatchGAN discriminator, VGG19 perceptual loss (Isola et al., 2017).
-"""
+"""U-Net generator, 70x70 PatchGAN discriminator, VGG19 perceptual loss (Isola et al., 2017)."""
 import torch
 import torch.nn as nn
 from torchvision import models
